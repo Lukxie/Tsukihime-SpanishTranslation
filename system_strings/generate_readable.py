74f28a765b1a019fe34c5c29857ed20e91716ca6
Lukxie <115797570+Lukxie@users.noreply.github.com>
2024-12-08 19:20:59 +0100

    Update generate_readable.py

--- system_strings/generate_readable.py
+++ system_strings/generate_readable.py
@@ -7,20 +7,20 @@ def main():
     with open(sys.argv[1], 'r') as f:
         jp_text = f.read()
     with open(sys.argv[2], 'r') as f:
-        en_text = f.read()
+        es_text = f.read()
 
     # Generate a map of SHA -> EN text
     line_by_sha = {}
     jp_line_by_sha = {}
-    for jp, en in zip(jp_text.split("\n"), en_text.split("\n")):
+    for jp, en in zip(jp_text.split("\n"), es_text.split("\n")):
         sha = hashlib.sha1(jp.encode('utf-8')).hexdigest()
-        line_by_sha[sha] = en
+        line_by_sha[sha] = es
         jp_line_by_sha[sha] = jp
 
     for sha, en in line_by_sha.items():
         print(f"[sha:{sha}]" + "{")
         print(f"-- {jp_line_by_sha[sha]}")
-        print(f"{en}")
+        print(f"{es}")
         print("}")
 
 if __name__ == '__main__':
