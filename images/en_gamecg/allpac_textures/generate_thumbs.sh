5c4a879200e808e683db39474e3722598baa3688
Luxkie <miguelniethernandez99@gmail.com>
2025-06-18 22:06:40 +0200

    Half-moon patch pending of Taikutsu revision

--- images/en_gamecg/allpac_textures/generate_thumbs.sh
+++ images/en_gamecg/allpac_textures/generate_thumbs.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e  # haz que el script se pare si hay errores
+
+mkdir -p thumb            # crea la carpeta si no existe
+
 for F in *.png; do
-  convert -resize 316x285 "$F" "thumb/$F"
+  convert "$F" -resize 316x285 "thumb/$F"
 done
